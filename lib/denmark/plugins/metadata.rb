puppetlabs/denmark

commit 2bbbc4b2a6e13666262c0637818bb96e1c1459d0

don't fail if there is only one single release

lib/denmark/plugins/metadata.rb
@@ -18,7 +18,11 @@ class Denmark::Plugins::Metadata
     response = Array.new
 
     release_date = Date.parse(mod.releases.first.updated_at).to_date
-    prev_release = Date.parse(mod.releases[1].updated_at).to_date
+    prev_release = unless mod.releases[1].nil?
+                     Date.parse(mod.releases[1].updated_at).to_date
+                   else
+                     nil
+                   end
     version      = mod.releases.first.version
     changelog    = mod.releases.first.changelog
 

commit 8b7962307db7eb5c48f4cee09dd325c50bab6f6d

also don't fail if there is no date of the previous release
Fixes: 2bbbc4b2a6e13666262c0637818bb96e1c1459d0

lib/denmark/plugins/metadata.rb
@@ -84,7 +84,7 @@ class Denmark::Plugins::Metadata
       }
     end
 
-    if (release_date - prev_release) > 365
+    if !prev_release.nil? && (release_date - prev_release) > 365
       response << {
         severity: :green,
         message: "There was a gap of at least a year between the last two releases.",